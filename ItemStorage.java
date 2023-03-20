235968474928637143a4954e7c4c2e94c4926907
laytonpoore <128392076+laytonpoore@users.noreply.github.com>
2023-03-20 10:55:30 -0400

    Create ItemStorage.java

---
+++ ItemStorage.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.HashMap;
+
+public class ItemStorage {
+
+    HashMap<String, Item> items = new HashMap<>();
+    private File file = new File("items.txt");
+
+    public ItemStorage() {
+
+        readObjectFromFile();
+    }
+
+    // Add an item to the array
+    public void addItem(Item item) {
+
+        items.put(item.getUpc(), item);
+        System.out.println(item.getDescription() + ", $" + item.getPrice());
+        System.out.println("Item " + items.size() + " has been added \n");
+
+        writeObjectToFile(item);
+    }
+
+    // Lookup an item in the array
+    public Item lookupItem(String upc) {
+        return items.get(upc);
+    }
+
+    // Delete an item from the array
+    public Item deleteItem(String upc) {
+        System.out.println("Deleted Item \n");
+        return items.remove(upc);
+    }
+
+    // Displays Items
+    public void displayItems() {
+
+        for (String upc : items.keySet()) {
+            System.out.println(upc + " - " + items.get(upc).getDescription() + " - $" + items.get(upc).getPrice());
+        }
+        System.out.println("");
+    }
+
+    // Writes an Item to file
+    public void writeObjectToFile(Item anItem) {
+        try {
+            ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file));
+            output.writeObject(anItem);
+            output.flush();
+            output.close();
+        } catch (IOException ioe) {
+            System.err.println("Error saving to file");
+        }
+    }
+
+    // Reads an Item from the file
+    public void readObjectFromFile() {
+
+        try {
+            if (file.exists()) {
+                ObjectInputStream input = new ObjectInputStream(new FileInputStream(file));
+                Item loadedItem = (Item) input.readObject();
+                while (loadedItem != null) {
+                    loadedItem = items.put(loadedItem.getUpc(), loadedItem);
+                }
+                input.close();
+            } else {
+                ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file));
+                output.close();
+            }
+        } catch (IOException ioe) {
+            System.err.println("Error opening file");
+        } catch (ClassNotFoundException cnfe) {
+            System.err.println("Item.java does not exist");
+        }
+    }
+}
